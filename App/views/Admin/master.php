97e8848df8108af8e1918c802a3402255c128cdf
Douglas Martins <douglas5422@outlook.com>
2024-08-15 21:05:16 -0300

    feat: First Commit

---
+++ App/views/Admin/master.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?></title>
+
+    <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid px-0">
+        <?php require __DIR__ . "/$view" ; ?>
+    </div>
+
+    <div id="alertaMsg" class="alert_content">
+        <div class="alerta">
+            <i class="icon_alerta bi"></i>
+            <div class="description_alert"></div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
